6308009838bbcca40eacc0b5f072b59594e8f644
salam <slmdawod@gmail.com>
2020-01-24 14:00:28 -0500

    added the database tables

---
+++ database/migrations/2020_01_24_183316_create_product_attributes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_attributes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('product_id');
+            $table->string('attr_name');
+            $table->string('attr_value');
+            $table->timestamps();
+
+           /* $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');*/
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_attributes');
+    }
+}
